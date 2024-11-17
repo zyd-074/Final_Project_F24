880d52e7b320965c780b8a17e452030938d7a843
Zhang Y.D. <cnfighteraimi@gmail.com>
2024-11-17 16:31:36 -0500

    Task #3: CreditCard

--- src/main/java/Vanier/CreditCard.java
+++ src/main/java/Vanier/CreditCard.java
@@ -11,35 +11,62 @@ public class CreditCard {
 
     /**
      * Constructor
-     * @param newCardHolder The owner of the card
+     * @param cardHolder The owner of the card
      * @param limit The Credit limit of the card <p>
-     * Balance initialized at 0 with a Money object
+     * Note: Balance initialized at 0 with a Money object
      */
-    public CreditCard(Person newCardHolder, Money limit) {
+    public CreditCard(Person cardHolder, Money limit) {
         balance = new Money(0);
         creditLimit = limit;
-        owner = newCardHolder;
+        owner = cardHolder;
     }
+
+    /**
+     * The getBalance Method
+     * @return The balance amount for the current Credit Card as a Money object
+     */
     public Money getBalance() {
-        this.balance = new Money(balance);
         return this.balance;
     }
+
+    /**
+     * The getCreditLimit Method
+     * @return The credit limit amount available for the cardholder
+     */
     public Money getCreditLimit() {
-        this.creditLimit = new Money(creditLimit);
         return this.creditLimit;
     }
+
+    /**
+     * The getPersonals Method
+     * @return A String which tells the information about the Cardholder
+     */
     public String getPersonals() {
         return this.owner.toString();
     }
+
+    /**
+     * The charge Method <p>
+     * Will update the balance and the credit limit after spending the amount.
+     * @param amount The amount that got spent by the Cardholder
+     */
     public void charge(Money amount){
-        if (this.balance.add(amount).compareTo(this.creditLimit) < 0){
+        if (this.balance.add(amount).compareTo(this.creditLimit) <= 0){
             this.balance = this.balance.add(amount);
+            this.creditLimit = this.creditLimit.subtract(amount);
         }
         else{
             System.out.println("Exceeds credit limit");
         }
     }
+
+    /**
+     * The payment Method <p>
+ *     Will update the balance and credit limit after repaying the amount.
+     * @param amount The amount that got repaid by the Cardholder
+     */
     public void payment(Money amount){
         this.balance = this.balance.subtract(amount);
+        this.creditLimit = this.creditLimit.add(amount);
     }
 }
