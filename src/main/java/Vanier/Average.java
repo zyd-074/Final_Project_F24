efb90e3423d4995c4ace217ad46519a73eca4b9d
Zhang Y.D. <cnfighteraimi@gmail.com>
2024-11-24 21:56:18 -0500

    Comment code + Fixed bug

--- src/main/java/Vanier/Average.java
+++ src/main/java/Vanier/Average.java
@@ -2,11 +2,16 @@ package Vanier;
 import java.util.Scanner;
 
 public class Average {
+    //Data Field
     private int[] data = new int[5];
     private double mean;
 
+    //Create Scanner to let user input data
     Scanner user = new Scanner(System.in);
 
+    /**
+     * The constructor
+     */
     public Average() {
         for (int num = 0; num < 5; num++) {
             System.out.print("Score number " + (num+1) + ": ");
@@ -15,27 +20,39 @@ public class Average {
         }
     }
 
+    /**
+     * The calculate Mean Method <p>
+     * Will calculate the Average of the scores entered by the User
+     */
     public void calculateMean() {
-        for (int num = 0; num < 5; num++) {
+        for (int num = 0; num < this.data.length; num++) {
             this.mean += this.data[num];
         }
         this.mean /= this.data.length;
     }
 
+    /**
+     * The selectionSort Method <p>
+     * Will rearrange the scores from highest to lowest
+     */
     public  void selectionSort() {
         for (int num1 = 0; num1 < this.data.length; num1++) {
-            int minIndex = num1;
+            int maxIndex = num1;
             for (int num2 = num1 + 1; num2 < this.data.length; num2++) {
-                if (this.data[num2] < this.data[minIndex]) {
-                    minIndex = num2;
+                if (this.data[num2] > this.data[maxIndex]) {
+                    maxIndex = num2;
                 }
             }
             int temp = this.data[num1];
-            this.data[num1] = this.data[minIndex];
-            this.data[minIndex] = temp;
+            this.data[num1] = this.data[maxIndex];
+            this.data[maxIndex] = temp;
         }
     }
 
+    /**
+     * The toString Method
+     * @return A string which shows the both the scores in order and the average
+     */
     public String toString() {
         String string = "The scores in order : %s \nThe average score is : %.2f";
         String scores = String.valueOf(this.data[0]);
