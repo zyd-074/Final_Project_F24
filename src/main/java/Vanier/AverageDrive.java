21724b25cb86ee130c715b0224f5fe12bb663628
Zhang Y.D. <cnfighteraimi@gmail.com>
2024-11-24 22:00:46 -0500

    Comment File (Task2)

--- src/main/java/Vanier/AverageDrive.java
+++ src/main/java/Vanier/AverageDrive.java
@@ -2,11 +2,14 @@ package Vanier;
 
 public class AverageDrive {
     public static void main(String[] args) {
+        //Create Object
         Average result = new Average();
 
+        //Call method to operate
         result.selectionSort();
         result.calculateMean();
 
+        //Create String to display result
         String display = result.toString();
         System.out.println(display);
     }
